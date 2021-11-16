61fbb3dbda4cb470e772e32f3fe306df8468c5e8
Kelsey Thompson <kelsthompson2@gmail.com>
2021-11-16 12:22:15 -0700

    Create/update facade methods

--- app/facades/mapquest_facade.rb
+++ app/facades/mapquest_facade.rb
@@ -4,5 +4,9 @@ class MapquestFacade
       result = MapquestService.get_geocode(location)[:results].first[:locations].first[:latLng]
       Location.new(result)
     end
+
+    def get_route_time(start_city, end_city)
+      MapquestService.get_directions(start_city, end_city)[:route][:formattedTime]
+    end
   end
 end
