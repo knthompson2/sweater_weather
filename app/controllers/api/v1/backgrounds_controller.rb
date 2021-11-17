5130b3944631ed13b82e05bec337b5b34a2e443e
Kelsey Thompson <kelsthompson2@gmail.com>
2021-11-16 17:39:48 -0700

    Update params variable to match requirements

--- app/controllers/api/v1/backgrounds_controller.rb
+++ app/controllers/api/v1/backgrounds_controller.rb
@@ -1,9 +1,9 @@
 class Api::V1::BackgroundsController < ApplicationController
   def index
-    if params[:query].empty?
+    if params[:location].nil?
       render json: {error: "bad-request"}, status: 400
     else
-      image = ImageFacade.get_image_info(params[:query])
+      image = ImageFacade.get_image_info(params[:location])
       render json: ImageSerializer.new(image), status: 200
     end
   end
